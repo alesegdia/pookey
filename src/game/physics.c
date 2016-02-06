d496abfe60f38f2f4d9afc8f470ded8d3ff12560
Alejandro Seguí <alesegdia@gmail.com>
2016-02-06 21:22:42 +0100

    placing physics functions outside main file

---
+++ src/game/physics.c
@@ -0,0 +1,25 @@
+
+#include "physics.h"
+
+#define POOKEY_GRAVITY 		0x0020
+
+int isGrounded( entity_t* e )
+{
+	return e->flags & 0x01;
+}
+
+void setGrounded( entity_t* e )
+{
+	e->flags |= 0x01;
+}
+
+void applyGravity( entity_t* e )
+{
+	e->speed.y.w += POOKEY_GRAVITY;
+}
+
+void applySpeed( entity_t* e )
+{
+	e->pos.x.w = e->pos.x.w + e->speed.x.w;
+	e->pos.y.w = e->pos.y.w + e->speed.y.w;
+}
---
+++ src/game/physics.h
@@ -0,0 +1,20 @@
+
+#ifndef __PHYSICS_H__
+#define __PHYSICS_H__
+
+#include "entity.h"
+
+// checks if grounded bit is active
+int isGrounded( entity_t* e );
+
+// sets the grounded bit to true
+void setGrounded( entity_t* e );
+
+// applies gravity to entity speed
+void applyGravity( entity_t* e );
+
+// applies entity speed to entity position
+void applySpeed( entity_t* e );
+
+#endif
+
