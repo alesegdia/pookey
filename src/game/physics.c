2dcc3a2daeeb7c3da4d53885ad5273966230ab09
Alejandro Seguí <alesegdia@gmail.com>
2016-02-12 21:07:43 +0100

    collisions working without scrolling

--- src/game/physics.c
+++ src/game/physics.c
@@ -27,40 +27,43 @@ void applySpeed( entity_t* e )
 
 void handleMapCollision( entity_t* e, map_t* map )
 {
-	UBYTE colx, coly;
+	UBYTE x, y,
+		  xmin, xmax,
+		  ymin, ymax,
+		  colx, coly,
+		  up;
+
+	fixed f;
 	colx = 0;
 	coly = 0;
 
 	//if( e->speed.x.w != 0 )
 	{
-		fixed f;
-		UBYTE off, x, y, ymin, ymax, extra;
-		ymin = (((int)e->pos.y.b.h) >> 3) + 2;
-		ymax = ymin + 1;
+		ymin = (((int)e->pos.y.b.h-2) >> 3) - 1;
+		ymax = ymin+1;
 		f.w = e->pos.x.w + e->speed.x.w;
-		x = f.b.h / 8 + 2;
 		if( e->speed.x.w > 0x7FFF )
 		{
+			x = (f.b.h + 4) >> 3;
 			for( y = ymin; y <= ymax; y++ )
 			{
 				unsigned char mt = get_tilemap_tile( map, x-1, y );
 				if( mt != 0 )
 				{
 					e->speed.x.w = 0;
-					e->pos.x.b.h = (x - 1) * 8 - 3;
 					colx = 1;
 				}
 			}
 		}
 		else if( e->speed.x.w > 0 )
 		{
+			x = (f.b.h - 0) >> 3;
 			for( y = ymin; y <= ymax; y++ )
 			{
 				unsigned char mt = get_tilemap_tile( map, x+1, y );
 				if( mt != 0 )
 				{
 					e->speed.x.w = 0;
-					e->pos.x.b.h = (x - 2) * 8 - 0;
 					colx = 1;
 				}
 			}
@@ -69,33 +72,28 @@ void handleMapCollision( entity_t* e, map_t* map )
 
 	//if( e->speed.y.w != 0 )
 	{
-		fixed f;
-		UBYTE offset_x_pixels, x, y, xmin, xmax, offset_y_tiles, up;
 		if( e->speed.y.w > 0x7FFF )
 		{
-			offset_y_tiles = 3;
 			up = 1;
 		}
 		else if( e->speed.y.w > 0 )
 		{
-			offset_y_tiles = 4;
 			up = 0;
 		}
-		offset_x_pixels = 8 * 9;
-		xmin = (((int)e->pos.x.b.h) + offset_x_pixels) >> 3;
+		xmin = ((int)e->pos.x.b.h) >> 3;
 		xmax = xmin + 1;
 		f.w = e->pos.y.w + e->speed.y.w;
-		y = f.b.h / 8 + offset_y_tiles;
+		y = (f.b.h - 1) / 8;
 		for( x = xmin; x <= xmax; x++ )
 		{
 			unsigned char mt;
 			if( up == 1 )
 			{
-				mt = get_tilemap_tile( map, x, y-1 );
+				mt = get_tilemap_tile( map, x, y-2 );
 			}
 			else
 			{
-				mt = get_tilemap_tile( map, x, y-1 );
+				mt = get_tilemap_tile( map, x, y );
 			}
 			if( mt != 0 )
 			{
@@ -103,7 +101,6 @@ void handleMapCollision( entity_t* e, map_t* map )
 				coly = 1;
 				if( up == 0 )
 				{
-					e->pos.y.b.h = (y - offset_y_tiles) * 8;
 					setGrounded(e);
 				}
 			}
