4a0c6532212f9e09436bacece285262a58397314
Alejandro Seguí <alesegdia@gmail.com>
2016-02-18 23:06:07 +0100

    removing block where collision check is inside of

--- src/game/physics.c
+++ src/game/physics.c
@@ -51,59 +51,55 @@ void handleMapCollision( entity_t* e, map_t* map )
 	x_collision_exists = 0;
 	y_collision_exists = 0;
 
-	//if( e->speed.x.w != 0 )
+	// Y COLLISION
+	y_min_tile = (((UBYTE)e->pos.y.b.h-1 + BOX_UP) >> 3) - 1;
+	y_max_tile = (((UBYTE)e->pos.y.b.h-1 + 8) >> 3) - 1;
+	f.w = e->pos.x.w + e->speed.x.w;
+	if( e->speed.x.w > 0x7FFF )
 	{
-		y_min_tile = (((UBYTE)e->pos.y.b.h-1 + BOX_UP) >> 3) - 1;
-		y_max_tile = (((UBYTE)e->pos.y.b.h-1 + 8) >> 3) - 1;
-		f.w = e->pos.x.w + e->speed.x.w;
-		if( e->speed.x.w > 0x7FFF )
-		{
-			x = ((f.b.h + XOFFSET + BOX_LEFT) >> 3) - 1;
-		}
-		else if( e->speed.x.w > 0 )
-		{
-			x = ((f.b.h + XOFFSET - BOX_RIGHT) >> 3) + 1;
-		}
+		x = ((f.b.h + XOFFSET + BOX_LEFT) >> 3) - 1;
+	}
+	else if( e->speed.x.w > 0 )
+	{
+		x = ((f.b.h + XOFFSET - BOX_RIGHT) >> 3) + 1;
+	}
 
-		for( y = y_min_tile; y <= y_max_tile; y++ )
+	for( y = y_min_tile; y <= y_max_tile; y++ )
+	{
+		unsigned char mt = get_tilemap_tile( map, x, y );
+		if( mt != 0 )
 		{
-			unsigned char mt = get_tilemap_tile( map, x, y );
-			if( mt != 0 )
-			{
-				e->speed.x.w = 0;
-				x_collision_exists = 1;
-			}
+			e->speed.x.w = 0;
+			x_collision_exists = 1;
 		}
 	}
 
-	//if( e->speed.y.w != 0 )
+	// X COLLISION
+	x_min_tile = ((UBYTE)e->pos.x.b.h + XOFFSET - BOX_LEFT) / 8;
+	x_max_tile = ((UBYTE)e->pos.x.b.h + XOFFSET + 8 - BOX_RIGHT) / 8;
+	f.w = e->pos.y.w + e->speed.y.w;
+	if( e->speed.y.w > 0x7FFF )
 	{
-		x_min_tile = ((UBYTE)e->pos.x.b.h + XOFFSET - BOX_LEFT) / 8;
-		x_max_tile = ((UBYTE)e->pos.x.b.h + XOFFSET + 8 - BOX_RIGHT) / 8;
-		f.w = e->pos.y.w + e->speed.y.w;
-		if( e->speed.y.w > 0x7FFF )
+		moving_up = 1;
+		y = (f.b.h - 16 + BOX_UP) >> 3;
+	}
+	else if( e->speed.y.w > 0 )
+	{
+		moving_up = 0;
+		y = (f.b.h) >> 3;
+	}
+	for( x = x_min_tile; x <= x_max_tile; x++ )
+	{
+		unsigned char mt = get_tilemap_tile( map, x, y );
+		if( mt != 0 )
 		{
-			moving_up = 1;
-			y = (f.b.h - 16 + BOX_UP) >> 3;
-		}
-		else if( e->speed.y.w > 0 )
-		{
-			moving_up = 0;
-			y = (f.b.h) >> 3;
-		}
-		for( x = x_min_tile; x <= x_max_tile; x++ )
-		{
-			unsigned char mt = get_tilemap_tile( map, x, y );
-			if( mt != 0 )
+			e->speed.y.w = 0;
+			y_collision_exists = 1;
+			if( moving_up == 0 )
 			{
-				e->speed.y.w = 0;
-				y_collision_exists = 1;
-				if( moving_up == 0 )
-				{
-					setGrounded(e);
-					e->pos.y.b.h = y * 8;
-					e->pos.y.b.l = 0;
-				}
+				setGrounded(e);
+				e->pos.y.b.h = y * 8;
+				e->pos.y.b.l = 0;
 			}
 		}
 	}
