90d40911dfa074792ebff64f7160a48ce72901f7
Alejandro Seguí <alesegdia@gmail.com>
2016-07-11 20:25:12 +0200

    changing start player pos

--- src/game/defs.h
+++ src/game/defs.h
@@ -10,7 +10,7 @@
 #define POOKEY_RUN_SPEED 	0x0200
 #define ACCEL 0x0020
 
-#define POOKEY_START_X 		0x0800
+#define POOKEY_START_X 		0x0000
 #define POOKEY_START_Y 		0x2000
 
 #define POOKEY_LEFT_CAM_OFFSET 	9
